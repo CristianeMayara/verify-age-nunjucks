ef17ea9767a53dc900c67e5dc3a7f69e5225cd5f
Cristiane Mayara <cris.mayara@gmail.com>
2019-03-10 14:37:48 -0300

    Initializing project

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const app = express()
+
+nunjucks.configure('views', {
+  autoscape: true,
+  express: app,
+  watch: true
+})
+
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
+
+app.get('/', (req, res) => {
+  return console.log('My app')
+})
+
+app.listen(3000)
